92ca32d98a0a746ec79e355a698995774ecb9e5a
dns3uae-glitch <dns3uae@gmail.com>
2025-10-12 15:31:09 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+
+// إعداد Firebase بالطريقة التقليدية
+const firebaseConfig = {
+  apiKey: "AIzaSyCFsgB9PD0nszKbMJPak0qA-erPgFs01ns",
+  authDomain: "crime-book1.firebaseapp.com",
+  projectId: "crime-book1",
+  storageBucket: "crime-book1.firebasestorage.app",
+  messagingSenderId: "1017769661825",
+  appId: "1:1017769661825:web:e0ccc3b5e9c4a74a9a4480"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
